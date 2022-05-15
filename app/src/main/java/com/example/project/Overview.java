49d5744e0e62c5cb2874225b4f7b594ee38151a8
Raman Mohammed <a21rammo@outlook.com>
2022-05-15 08:45:35 +0200

    Added overview  activity and  changed a bit in login page

--- app/src/main/java/com/example/project/Overview.java
+++ app/src/main/java/com/example/project/Overview.java
@@ -1,16 +1,14 @@
 package com.example.project;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-public class MainActivity extends AppCompatActivity {
+public class Overview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_overview);
     }
-
 }
