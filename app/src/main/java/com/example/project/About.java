089e6543fc43d4a077f963cd94ad4046e0a23199
Raman Mohammed <a21rammo@outlook.com>
2022-05-23 20:01:34 +0200

    Fixed all the return to main page buttons with intents.

--- app/src/main/java/com/example/project/About.java
+++ app/src/main/java/com/example/project/About.java
@@ -2,13 +2,30 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
+    private Button aboutBack_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        aboutBack_btn = findViewById(R.id.aboutBack_btn);
+        aboutBack_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackButton();
+            }
+        });
+    }
+
+    private void onBackButton() {
+        Intent intent = new Intent(About.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,6 +23,7 @@ public class SecondActivity extends AppCompatActivity implements JsonTask.JsonTa
     private ArrayList<ProgrammingLanguages> mProgrammingData;
     private Gson gson;
     private Type type;
+    private Button detailsBack_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,19 @@ public class SecondActivity extends AppCompatActivity implements JsonTask.JsonTa
 
         new JsonTask(this).execute(JSON_URL);
 
+        detailsBack_btn = findViewById(R.id.back_btn);
+        detailsBack_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackButton();
+            }
+        });
+
+    }
+
+    private void onBackButton() {
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
